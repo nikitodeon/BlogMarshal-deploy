cb8b4e89805a5f979abfcbef69fecf8a92638988
Nikita <ragerage333@gmail.com>
2025-01-07 02:59:43 +0300

    10.33

--- app/dashboard/pricing/page.tsx
+++ app/dashboard/pricing/page.tsx
@@ -2,7 +2,7 @@ import { SubmitButton } from "@/app/components/dashboard/SubmitButtons";
 import { PricingTable } from "@/app/components/shared/Pricing";
 import { prisma } from "@/app/utils/db";
 import { requireUser } from "@/app/utils/requireUser";
-// import { stripe } from "@/app/utils/stripe";
+import { stripe } from "@/app/utils/stripe";
 import {
   Card,
   CardContent,
@@ -12,61 +12,61 @@ import {
 } from "@/components/ui/card";
 import { redirect } from "next/navigation";
 
-// async function getData(userId: string) {
-//   const data = await prisma.subscription.findUnique({
-//     where: {
-//       userId: userId,
-//     },
-//     select: {
-//       status: true,
-//       User: {
-//         select: {
-//           customerId: true,
-//         },
-//       },
-//     },
-//   });
+async function getData(userId: string) {
+  const data = await prisma.subscription.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      status: true,
+      User: {
+        select: {
+          customerId: true,
+        },
+      },
+    },
+  });
 
-//   return data;
-// }
+  return data;
+}
 
 export default async function PricingPage() {
   const user = await requireUser();
-  //   const data = await getData(user.id);
+  const data = await getData(user.id);
 
-  //   async function createCustomerPortal() {
-  //     "use server";
+  async function createCustomerPortal() {
+    "use server";
 
-  //     const session = await stripe.billingPortal.sessions.create({
-  //       customer: data?.User?.customerId as string,
-  //       return_url:
-  //         process.env.NODE_ENV === "production"
-  //           ? "https://blog-marshal.vercel.app/dashboard"
-  //           : "http://localhost:3000/dashboard",
-  //     });
+    const session = await stripe.billingPortal.sessions.create({
+      customer: data?.User?.customerId as string,
+      return_url:
+        process.env.NODE_ENV === "production"
+          ? "https://blog-marshal.vercel.app/dashboard"
+          : "http://localhost:3000/dashboard",
+    });
 
-  //     return redirect(session.url);
-  //   }
+    return redirect(session.url);
+  }
 
-  //   if (data?.status === "active") {
-  //     return (
-  //       <Card className="w-full ">
-  //         <CardHeader>
-  //           <CardTitle>Edit Subscription</CardTitle>
-  //           <CardDescription>
-  //             Click on the button below, this will give you the opportunity to
-  //             change your payment details and view your statement at the same
-  //             time.
-  //           </CardDescription>
-  //         </CardHeader>
-  //         <CardContent>
-  //           <form action={createCustomerPortal}>
-  //             <SubmitButton text="View Subscription Details" />
-  //           </form>
-  //         </CardContent>
-  //       </Card>
-  //     );
-  //   }
+  if (data?.status === "active") {
+    return (
+      <Card className="w-full ">
+        <CardHeader>
+          <CardTitle>Edit Subscription</CardTitle>
+          <CardDescription>
+            Click on the button below, this will give you the opportunity to
+            change your payment details and view your statement at the same
+            time.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={createCustomerPortal}>
+            <SubmitButton text="View Subscription Details" />
+          </form>
+        </CardContent>
+      </Card>
+    );
+  }
 
   return (
     <div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import {
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-// import { Hero } from "./components/frontend/Hero";
+import { Hero } from "./components/frontend/Hero";
 // import { Logos } from "./components/frontend/Logos";
 // import { Features } from "./components/frontend/Features";
 // import { PricingTable } from "./components/shared/Pricing";
@@ -22,7 +22,8 @@ export default async function Home() {
   return (
     <>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
-        {/* <Hero />
+        <Hero />
+        {/*
       <Logos />
       <Features />
       <PricingTable /> */}
