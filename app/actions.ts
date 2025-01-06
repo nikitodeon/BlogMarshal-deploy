e933eb735bb4538f6c777430f68e68f22b6d6379
Nikita <ragerage333@gmail.com>
2025-01-06 09:33:28 +0300

    9.00

--- app/actions.ts
+++ app/actions.ts
@@ -6,7 +6,7 @@ import { parseWithZod } from "@conform-to/zod";
 import { PostSchema, SiteCreationSchema, siteSchema } from "./utils/zodSchemas";
 import { prisma } from "./utils/db";
 import { requireUser } from "./utils/requireUser";
-// import { stripe } from "./utils/stripe";
+import { stripe } from "./utils/stripe";
 
 export async function CreateSiteAction(prevState: any, formData: FormData) {
   const user = await requireUser(); // Проверяем авторизацию пользователя
@@ -143,55 +143,55 @@ export async function DeleteSite(formData: FormData) {
   return redirect("/dashboard/sites");
 }
 
-// export async function CreateSubscription() {
-//   const user = await requireUser();
+export async function CreateSubscription() {
+  const user = await requireUser();
 
-//   let stripeUserId = await prisma.user.findUnique({
-//     where: {
-//       id: user.id,
-//     },
-//     select: {
-//       customerId: true,
-//       email: true,
-//       firstName: true,
-//     },
-//   });
+  let stripeUserId = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+    select: {
+      customerId: true,
+      email: true,
+      firstName: true,
+    },
+  });
 
-//   if (!stripeUserId?.customerId) {
-//     const stripeCustomer = await stripe.customers.create({
-//       email: stripeUserId?.email,
-//       name: stripeUserId?.firstName,
-//     });
+  if (!stripeUserId?.customerId) {
+    const stripeCustomer = await stripe.customers.create({
+      email: stripeUserId?.email,
+      name: stripeUserId?.firstName,
+    });
 
-//     stripeUserId = await prisma.user.update({
-//       where: {
-//         id: user.id,
-//       },
-//       data: {
-//         customerId: stripeCustomer.id,
-//       },
-//     });
-//   }
+    stripeUserId = await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        customerId: stripeCustomer.id,
+      },
+    });
+  }
 
-//   const session = await stripe.checkout.sessions.create({
-//     customer: stripeUserId.customerId as string,
-//     mode: "subscription",
-//     billing_address_collection: "auto",
-//     payment_method_types: ["card"],
-//     line_items: [{ price: process.env.STRIPE_PRICE_ID, quantity: 1 }],
-//     customer_update: {
-//       address: "auto",
-//       name: "auto",
-//     },
-//     success_url:
-//       process.env.NODE_ENV === "production"
-//         ? "https://blog-marshal.vercel.app/dashboard/payment/success"
-//         : "http://localhost:3000/dashboard/payment/success",
-//     cancel_url:
-//       process.env.NODE_ENV === "production"
-//         ? "https://blog-marshal.vercel.app/dashboard/payment/cancelled"
-//         : "http://localhost:3000/dashboard/payment/cancelled",
-//   });
+  const session = await stripe.checkout.sessions.create({
+    customer: stripeUserId.customerId as string,
+    mode: "subscription",
+    billing_address_collection: "auto",
+    payment_method_types: ["card"],
+    line_items: [{ price: process.env.STRIPE_PRICE_ID, quantity: 1 }],
+    customer_update: {
+      address: "auto",
+      name: "auto",
+    },
+    success_url:
+      process.env.NODE_ENV === "production"
+        ? "https://blog-marshal.vercel.app/dashboard/payment/success"
+        : "http://localhost:3000/dashboard/payment/success",
+    cancel_url:
+      process.env.NODE_ENV === "production"
+        ? "https://blog-marshal.vercel.app/dashboard/payment/cancelled"
+        : "http://localhost:3000/dashboard/payment/cancelled",
+  });
 
-//   return redirect(session.url as string);
-// }
+  return redirect(session.url as string);
+}
