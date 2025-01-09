5a58ee26d46e1f558411bae8fdd83d63c2da75b8
Nikita <ragerage333@gmail.com>
2025-01-10 02:13:58 +0300

    fix redirect6

--- app/utils/requireUser.ts
+++ app/utils/requireUser.ts
@@ -1,12 +1,12 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 // import { NextResponse } from "next/server";
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export const requireUser = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-  // if (!user) {
-  //   return redirect("/api/auth/login");
-  // }
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
   return user;
 };
